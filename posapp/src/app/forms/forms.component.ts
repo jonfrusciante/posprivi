778b851477f1b0bf0f0edbec27494de7ff460003
gino <ginocampo@gmail.com>
2018-05-15 19:00:48 +0200

    testing

--- posapp/src/app/forms/forms.component.ts
+++ posapp/src/app/forms/forms.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from "@angular/forms";
-
+import { AngularFirestore,AngularFirestoreCollection } from 'angularfire2/firestore';
+import {Observable} from "rxjs/index";
+export  interface Prodotti {
+  nome:string;
+  categoria:string;
+  prezzo:number;
+}
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css']
 })
 export class FormsComponent implements OnInit {
+  ProdottiCollectionRef:AngularFirestoreCollection<Prodotti>;
+  Prodotti$:Observable<Prodotti[]>;
+  Prodotto:Prodotti;
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {
+this.ProdottiCollectionRef =this.afs.collection( 'Prodotti');
+  }
+  addPrdotti(regForm:NgForm){
+
+  }
   registerUser(regForm:NgForm) {
-    console.log(regForm);
+    this.Prodotto.nome=regForm.value.nameitems;
+    this.Prodotto.categoria=regForm.value.Categoria;
+    this.Prodotto.prezzo=regForm.value.prezzo;
+    console.log(this.Prodotto);
+
+    this.ProdottiCollectionRef.add(this.Prodotto)
   }
   ngOnInit() {
   }
