2f4efd42b1d817ccab65cb7cd56e78f514a31727
gino <ginocampo@gmail.com>
2018-05-16 11:16:29 +0200

    testing

--- posapp/src/app/forms/forms.component.ts
+++ posapp/src/app/forms/forms.component.ts
@@ -39,9 +39,9 @@ export class FormsComponent implements OnInit {
     this.Prodotto.nome=regForm.value.nameitems;
     this.Prodotto.categoria=regForm.value.selectedCat;
     this.Prodotto.prezzo=regForm.value.prezzo;
-    console.log(this.Prodotto);
+    console.log(regForm);
 
-    this.ProdottiCollectionRef.add(this.Prodotto)
+    //this.ProdottiCollectionRef.add(this.Prodotto)
   }
   ngOnInit() {
     this.Categorie$=this.CategCollRef.valueChanges();
