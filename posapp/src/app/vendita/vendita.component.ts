ba3295bcc4d9cd749a40b7568441f2a5eb4fe0a8
gino <ginocampo@gmail.com>
2018-05-17 23:34:07 +0200

    testing

--- posapp/src/app/vendita/vendita.component.ts
+++ posapp/src/app/vendita/vendita.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs/Rx";
 import {AngularFirestore} from "angularfire2/firestore";
+import {Prodotti} from "../forms/forms.component";
+import {map} from "rxjs/internal/operators";
 
 @Component({
   selector: 'app-vendita',
@@ -9,13 +11,22 @@ import {AngularFirestore} from "angularfire2/firestore";
 })
 export class VenditaComponent implements OnInit {
   categories:Observable<any[]>;
-  order:Object={};
+  order=[];
   Product:Observable<any[]>;
   constructor(private  afs:AngularFirestore) {
     this.categories = this.afs.collection('Categorie_prodotti').valueChanges();
-    this.Product=this.afs.collection('Prodotti').valueChanges();
+    this.Product=this.afs.collection('Prodotti').snapshotChanges().pipe(
+      map(actions => actions.map( a =>
+      {
+        const data = a.payload.doc.data() as Prodotti;
+        const id = a.payload.doc.id;
+        return {id, ...data};
+      }))
+    );
   }
-  filtrati(item){
+  addproduct(productId){
+    this.order.push(
+    this.afs.collection('Prodotti').doc(productId).valueChanges());
 
   }
   ngOnInit() {
