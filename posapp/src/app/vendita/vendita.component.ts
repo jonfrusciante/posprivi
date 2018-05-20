63eb4a0be133f7cf0a90fff0c7e8d47632fa4b14
gino <ginocampo@gmail.com>
2018-05-20 15:12:41 +0200

    orderticket

--- posapp/src/app/vendita/vendita.component.ts
+++ posapp/src/app/vendita/vendita.component.ts
@@ -1,34 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from "rxjs/Rx";
-import {AngularFirestore} from "angularfire2/firestore";
-import {Prodotti} from "../forms/forms.component";
-import {map} from "rxjs/internal/operators";
-export interface OrderTicket{
-  id?:string;
-  nTavolo:string;
-  NCoperti:number;
-  ordine:Object;
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {AngularFirestore} from 'angularfire2/firestore';
+import {Prodotti} from '../forms/forms.component';
+import {map} from 'rxjs/internal/operators';
+import {ActivatedRoute, Router} from '@angular/router';
+export interface OrderTicket {
+  id?: string;
+  nTavolo: string;
+  NCoperti: number;
+  ordine: Object;
 }
 @Component({
   selector: 'app-vendita',
   templateUrl: './vendita.component.html',
   styleUrls: ['./vendita.component.css']
 })
-export class VenditaComponent implements OnInit {
-  nCoperti:number;
-  selectedtableId:any;
+ export class VenditaComponent implements OnInit , OnDestroy {
+  nCoperti: number;
+  selectedtableId: any;
   public now: Date = new Date();
-  Table$:Observable<any[]>;
-  categories:Observable<any[]>;
-  order=[];
-  Prodotto$:Observable<any>;
-  Product:Observable<any[]>;
-  constructor(private  afs:AngularFirestore) {
-    this.Table$=this.afs.collection('Tavoli').valueChanges();
+  Table$: Observable<any[]>;
+  categories: Observable<any[]>;
+  order = [];
+  Prodotto$: Observable<any>;
+  Product: Observable<any[]>;
+  private sub: any;
+
+  constructor(private  afs: AngularFirestore , private route: ActivatedRoute,
+              private router: Router) {
+    this.Table$ = this.afs.collection('Tavoli').valueChanges();
     this.categories = this.afs.collection('Categorie_prodotti').valueChanges();
-    this.Product=this.afs.collection('Prodotti').snapshotChanges().pipe(
-      map(actions => actions.map( a =>
-      {
+    this.Product = this.afs.collection('Prodotti').snapshotChanges().pipe(
+      map(actions => actions.map( a => {
         const data = a.payload.doc.data() as Prodotti;
         const id = a.payload.doc.id;
         return {id, ...data};
@@ -36,11 +39,11 @@ export class VenditaComponent implements OnInit {
     );
     setInterval(() => {
       this.now = new Date();
-    }, 1)
+    }, 1);
   }
-  addproduct(productId){
+  addproduct(productId) {
     this.afs.collection('Prodotti').doc<Prodotti>(productId).valueChanges().subscribe(s => {
-      s.id=productId;
+      s.id = productId;
       this.order.push(s);
     });
 
@@ -48,12 +51,21 @@ export class VenditaComponent implements OnInit {
 
   }
   ngOnInit() {
+    this.sub = this.route
+      .queryParams
+      .subscribe(params => {
+        // Defaults to 0 if no query param provided.
+        this.selectedtableId = params['table'] || undefined;
+      });
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
   getSum() {
     let i = 0 ,
       sum = 0;
-    for(; i < this.order.length; i++) {
+    for (; i < this.order.length; i++) {
       sum += parseInt(this.order[i].prezzo , 10);
     }
     return sum;
@@ -61,28 +73,28 @@ export class VenditaComponent implements OnInit {
   }
 
   deleteItem(i) {
-    this.order.splice(i,1);
+    this.order.splice(i, 1);
   }
 
   clearOrder() {
-    this.order=[];
+    this.order = [];
   }
 
   checkout(order) {
-    let c={
+    const c = {
       ordine : order ,
       nTavolo : this.selectedtableId,
-      status:'open',
-      data_modifica:this.now
+      status: 'open',
+      data_modifica: this.now
 
     };
     this.afs.collection('OrderTicket').add(c);
-    this.afs.collection('Tavoli').doc(c.nTavolo).update({islibero:false});
+    this.afs.collection('Tavoli').doc(c.nTavolo).update({islibero: false});
     this.afs.collection('Tavoli').doc(c.nTavolo).collection('ordini').add(c);
 
     console.log(c);
 
-    this.selectedtableId=undefined;
-    this.order=[];
+    this.selectedtableId = undefined;
+    this.order = [];
   }
 }
