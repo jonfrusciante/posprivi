de60065f1ec23843071042702433bb253e917a4f
gino <ginocampo@gmail.com>
2018-05-21 23:06:23 +0200

    orderticket

--- posapp/src/app/printer.service.ts
+++ posapp/src/app/printer.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs/Rx";
+import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
+import * as qz from 'qz-tray';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +12,37 @@ import { Injectable } from '@angular/core';
 export class PrinterService {
 
   constructor() { }
+  errorHandler(error: any): Observable<any> {
+    return Observable.throw(error);
+  }
+
+// Get list of printers connected
+  getPrinters(): Observable<string[]> {
+    return Observable
+      .fromPromise(qz.websocket.connect().then(() => qz.printers.find()))
+      .map((printers: string[]) => printers)
+      .catch(this.errorHandler);
+  }
+
+// Get the SPECIFIC connected printer
+  getPrinter(printerName: string): Observable<string> {
+    return Observable
+      .fromPromise(qz.websocket.connect().then(() => qz.printers.find(printerName)))
+      .map((printer: string) => printer)
+      .catch(this.errorHandler);
+  }
+
+// Print data to chosen printer
+  printData(printer: string, data: any): Observable<any> {
+    // Create a default config for the found printer
+    const config = qz.configs.create(printer);
+    return Observable.fromPromise(qz.print(config, data))
+      .map((anything: any) => anything)
+      .catch(this.errorHandler);
+  }
+
+// Disconnect QZ Tray from the browser
+  removePrinter(): void {
+    qz.websocket.disconnect();
+  }
 }
