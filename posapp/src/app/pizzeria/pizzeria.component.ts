c59546424f1ad96285712984e245c394e9a56cb8
gino <ginocampo@gmail.com>
2018-05-22 01:31:21 +0200

    orderticket

--- posapp/src/app/pizzeria/pizzeria.component.ts
+++ posapp/src/app/pizzeria/pizzeria.component.ts
@@ -7,13 +7,16 @@ import {PrinterService} from "../printer.service";
   styleUrls: ['./pizzeria.component.css']
 })
 export class PizzeriaComponent implements OnInit {
+  rep: string ; host: string;
 printerAvia:any;
   constructor(public  prinSer: PrinterService) {
     this.printerAvia = prinSer.printerAviable;
     console.log(this.printerAvia);
     prinSer.getPrinters().subscribe(n => console.log( n));
   }
-
+  addStampante() {
+    this.prinSer.setConfigPrinter(this.host, this.rep);
+  }
   ngOnInit() {
   }
 
