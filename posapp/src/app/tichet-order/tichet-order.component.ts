a00c22d288c587584c5db498613d654e5f38a3b9
gino <ginocampo@gmail.com>
2018-05-22 19:07:05 +0200

    orderticket

--- posapp/src/app/tichet-order/tichet-order.component.ts
+++ posapp/src/app/tichet-order/tichet-order.component.ts
@@ -33,7 +33,7 @@ export class TichetOrderComponent implements OnInit {
   }
   stampa() {
     const data = this.div.nativeElement.innerHTML;
-    console.log(data);
+    console.log(data.div);
     // this.prinSer.printFinal(data);
 
 
