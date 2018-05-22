4c5ae04a7c6b13715e39a549270a46aa5aee0618
gino <ginocampo@gmail.com>
2018-05-22 18:04:39 +0200

    orderticket

--- posapp/src/app/pizzeria/pizzeria.component.ts
+++ posapp/src/app/pizzeria/pizzeria.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PrinterService} from '../printer.service';
 import {Observable} from 'rxjs/Rx';
-import {map} from "rxjs/internal/operators";
 
 @Component({
   selector: 'app-pizzeria',
@@ -33,6 +32,6 @@ export class PizzeriaComponent implements OnInit {
       'More Raw Data\n',
       'Even More Raw Data\n'
     ];
-  this.prinSer.schegli('pizzeria').pipe( map ( n => this.prinSer.printData(n.printer, data).subscribe())) ; //  this.prinSer.printData()
+  this.prinSer.schegli('pizzeria').subscribe(n => this.prinSer.printData(n.printer, data).subscribe(nn => console.log(nn))) ; //  this.prinSer.printData()
   }
 }
