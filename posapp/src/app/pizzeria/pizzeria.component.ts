9f7ded3cb5aa51d2e8acc7c579918ad8bc0353e9
gino <ginocampo@gmail.com>
2018-05-23 15:45:10 +0200

    orderticket

--- posapp/src/app/pizzeria/pizzeria.component.ts
+++ posapp/src/app/pizzeria/pizzeria.component.ts
@@ -3,28 +3,43 @@ import {PrinterService} from '../printer.service';
 import {Observable} from 'rxjs/Rx';
 import {AngularFirestore} from "angularfire2/firestore";
 import {map} from "rxjs/internal/operators";
+import Order = jasmine.Order;
 
 @Component({
   selector: 'app-pizzeria',
   templateUrl: './pizzeria.component.html',
   styleUrls: ['./pizzeria.component.css']
 })
+export interface Ordine {
+  categoria:string;
+  nome:string;
+  prezzo:number;
+  id?:string;
+  printer:string;
+}
+export interface TicketOrder{
+  data_modifica:Date;
+  id?:string;
+  ordine: Ordine[];
+  status:string;
+
+}
 export class PizzeriaComponent implements OnInit {
   @ViewChild('ticket') div: ElementRef;
-  orders$: Observable<any[]>;
+  orders$: Observable<TicketOrder[]>;
   stampanteSel: string;
   rep: string;
   host: string;
   printerAvia: Observable<any[]>;
   installedPrinter: Observable<any[]>;
-  order:any;
+  order: TicketOrder[];
 
   constructor(private  prinSer: PrinterService, private afs: AngularFirestore) {
     this.printerAvia = prinSer.printerAviable;
     this.installedPrinter = prinSer.getPrinters();
     this.orders$ = this.afs.collection('Tavoli').doc('3' ).collection('ordini').snapshotChanges().pipe(
       map(actions => actions.map( a => {
-        const data = a.payload.doc.data() ;
+        const data = a.payload.doc.data() as TicketOrder ;
         const id = a.payload.doc.id;
         return {id, ...data};
       }))
@@ -44,7 +59,7 @@ export class PizzeriaComponent implements OnInit {
   }
 
   printP() {
-    console.log(this.order.ordine);
+    console.log(this.order.map(n => console.log(n.ordine)));
     const data = this.div.nativeElement.innerHTML;
      // this.prinSer.printFinal(data);
   }
