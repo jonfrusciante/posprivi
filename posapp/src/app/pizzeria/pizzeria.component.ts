f8d48ba630ae29ad91659286dcda1956c4301c5b
gino <ginocampo@gmail.com>
2018-05-23 16:46:42 +0200

    orderticket

--- posapp/src/app/pizzeria/pizzeria.component.ts
+++ posapp/src/app/pizzeria/pizzeria.component.ts
@@ -68,9 +68,10 @@ export class PizzeriaComponent implements OnInit {
 <html>
 <head>
 <title>Title of the document</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
