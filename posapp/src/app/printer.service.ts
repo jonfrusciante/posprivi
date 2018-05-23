f9aaa4ba3bbc59ecf3ee61abd7376c9ddd9a1abf
gino <ginocampo@gmail.com>
2018-05-23 17:07:55 +0200

    orderticket

--- posapp/src/app/printer.service.ts
+++ posapp/src/app/printer.service.ts
@@ -32,7 +32,7 @@ export class PrinterService {
       format: 'plain', // or 'plain' if the data is raw HTML
       data: ticketToPrint
     }];
-     this.schegli('pizzeria').subscribe(n => this.printDataPizz(n.printer, data).subscribe(nn => console.log(nn))) ; //  this.prinSer.printData()
+     this.schegli('pizzeria').subscribe(n => this.printData(n.printer, data).subscribe(nn => console.log(nn))) ; //  this.prinSer.printData()
   }
 
 
@@ -74,7 +74,7 @@ export class PrinterService {
     // Create a default config for the found printer
 
     const config = qz.configs.create(printer);
-    return Observable.fromPromise(qz.websocket.connect().then(() => qz.print(config, data)))
+    return Observable.fromPromise(qz.websocket.connect({host : '192.168.1.7'}).then(() => qz.print(config, data)))
       .map((anything: any) => anything)
       .catch(this.errorHandler);
   }
